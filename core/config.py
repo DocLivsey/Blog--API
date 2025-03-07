888cecbdd97bce7cc7db46c7858d514725ad768d
DocLivsey <doclivsey@users.noreply.github.com>
2025-03-07 22:53:44 +0300

    fix: sqlalchemy could not resolve 'db' container ref

--- core/config.py
+++ core/config.py
@@ -4,7 +4,10 @@ from os import environ
 
 
 class Settings(BaseSettings):
-    DATASOURCE_URL: str = environ.get('DATASOURCE_URL', 'postgresql://admin:password@db:5432/blog')
+    DATASOURCE_URL: str = environ.get('DATASOURCE_URL', 'postgresql://admin:password@localhost:5432/blog')
     REDIS_URL: str = environ.get('REDIS_URL', 'redis://localhost:6379')
 
 settings = Settings()
+
+if 'postgresql://' not in settings.DATASOURCE_URL:
+    settings.DATASOURCE_URL = f'postgresql://admin:password@{settings.DATASOURCE_URL}:5432/blog'
