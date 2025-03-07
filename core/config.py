45d27cf4ffd0869b827347dada2d727ebf2cbcdc
DocLivsey <doclivsey@users.noreply.github.com>
2025-03-07 22:22:37 +0300

    fix: improve old default value for connection

--- core/config.py
+++ core/config.py
@@ -4,7 +4,7 @@ from os import environ
 
 
 class Settings(BaseSettings):
-    DATASOURCE_URL: str = environ.get('DATASOURCE_URL', 'postgres://localhost:5432')
+    DATASOURCE_URL: str = environ.get('DATASOURCE_URL', 'postgresql://admin:password@db:5432/blog')
     REDIS_URL: str = environ.get('REDIS_URL', 'redis://localhost:6379')
 
 settings = Settings()
