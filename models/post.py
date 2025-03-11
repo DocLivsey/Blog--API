b7ef5335c475a688e680ccdb5d4cfd79a428d35c
DocLivsey <doclivsey@users.noreply.github.com>
2025-03-11 19:46:33 +0300

    fix: 'Post' object has no attribute 'dict'

--- models/post.py
+++ models/post.py
@@ -6,3 +6,6 @@ class Post(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     content = Column(String)
+
+    def dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.c}
